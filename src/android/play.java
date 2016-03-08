4858448a70616a46c084aa67c60929ee6eb59b7b
pearl <lu92500@yahoo.com.tw>
2016-03-08 16:32:49 +0800

    Update play.java

--- src/android/play.java
+++ src/android/play.java
@@ -2,7 +2,7 @@ package tw.com.bais.videoview2;
 
 //change this (com.phonegap.helloworld) to your package name, keep the .R
 //example: your.package.name.R;
-
+import tw.com.bais.*;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
