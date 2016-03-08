8eb169e689248f535f19f8179be2ec87ff2f23dc
pearl <lu92500@yahoo.com.tw>
2016-03-08 16:16:48 +0800

    Update videoplugin.java

--- src/android/videoplugin.java
+++ src/android/videoplugin.java
@@ -11,21 +11,36 @@ import android.content.Intent;
  * This class echoes a string called from JavaScript.
  */
 public class videoplugin extends CordovaPlugin {
-
+    private CallbackContext callbackContext = null;
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
         if (action.equals("play")) {
-            final String url= args.getString(0);  
-            final boolean showAds= args.getBoolean(1); 
-            final boolean isLive= args.getBoolean(2);
-            final String adServer = args.getString(3);
+            this.callbackContext = callbackContext;
+            CordovaResourceApi resourceApi = webView.getResourceApi();
+            String target = args.getString(0);
+            
+            //final String url= args.getString(0);  
+            final boolean showAds= false; 
+            final boolean isLive=  false;
+            final String adServer = "";
+            
+            final JSONObject options = args.getJSONObject(1);
+        
+            String fileUriStr;
+            try {
+                Uri targetUri = resourceApi.remapUri(Uri.parse(target));
+                fileUriStr = targetUri.toString();
+            } catch (IllegalArgumentException e) {
+                fileUriStr = target;
+            }
+            final String path = stripFileProtocol(fileUriStr);
             
             cordova.getActivity().runOnUiThread(new Runnable() {
                     @Override
                     public void run() {
                         Context context = cordova.getActivity().getApplicationContext();
                         Intent intent = new Intent(context,play.class);
-                        intent.putExtra("url", url);
+                        intent.putExtra("url", path);
                         intent.putExtra("showAds", showAds);
                         intent.putExtra("isLive", isLive);
                         intent.putExtra("adServer", adServer);
@@ -36,6 +51,13 @@ public class videoplugin extends CordovaPlugin {
                 return true;
         }
         return false;
+    }
+    
+    public static String stripFileProtocol(String uriString) {
+        if (uriString.startsWith("file://")) {
+            return Uri.parse(uriString).getPath();
+        }
+        return uriString;
     }
  
 }
