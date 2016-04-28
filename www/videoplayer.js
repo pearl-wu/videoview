29bb18ee2caeb222f4672fd53e887e8630d394db
pearl <lu92500@yahoo.com.tw>
2016-04-28 16:50:54 +0800

    Update videoplayer.js

--- www/videoplayer.js
+++ www/videoplayer.js
@@ -2,28 +2,29 @@ var exec = require("cordova/exec");
 
 module.exports = {
 
-    DEFAULT_OPTIONS: {
-        volume: 1.0,
-        scalingMode: 1
-    },
-
-    SCALING_MODE: {
-        SCALE_TO_FIT: 1,
-        SCALE_TO_FIT_WITH_CROPPING: 2
-    },
-
-    play: function (path, options, successCallback, errorCallback) {
-        options = this.merge(this.DEFAULT_OPTIONS, options);
-        exec(successCallback, errorCallback, "VideoPlayer", "play", [path, options]);
+    _OPTIONS: {
+        widthV: 1028,
+        heightV: 760,
+        topV: 0,
+        leftV: 0
     },
     
-    preview: function(path, options, successCallback, errorCallback){
-        options = this.merge(this.DEFAULT_OPTIONS, options); 
-        exec(successCallback, errorCallback, "VideoPlayer", "preview", [path, options]);
+    beginning: function (options, successCallback, errorCallback) {
+      options = this.merge(this._OPTIONS, options);  
+        exec(successCallback, errorCallback, "VideoPlayer", "beginning", [options]);
+    },
+    
+    many_play: function (paths, number, successCallback, errorCallback) {
+       
+        exec(successCallback, errorCallback, "VideoPlayer", "many_play", [paths, number]);
     },
 
-    close: function (successCallback, errorCallback) {
-        exec(successCallback, errorCallback, "VideoPlayer", "close", []);
+    play: function (path, successCallback, errorCallback) {
+        exec(successCallback, errorCallback, "VideoPlayer", "play", [path]);
+    },
+
+    stop: function (successCallback, errorCallback) {
+        exec(successCallback, errorCallback, "VideoPlayer", "stop", []);
     },
 
     merge: function () {
@@ -35,5 +36,4 @@ module.exports = {
         });
         return obj;
     }
-
 };
