716cd1cb0d34dd4019f9f61511f84c4e7553ef0a
pearl <lu92500@yahoo.com.tw>
2016-06-02 15:33:28 +0800

    Update VideoPlayerActivity.java

--- src/android/VideoPlayerActivity.java
+++ src/android/VideoPlayerActivity.java
@@ -30,7 +30,6 @@ import android.view.animation.AnimationUtils;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.Toast;
-import tw.com.bais.demoview.R;
 
 public class VideoPlayerActivity extends Activity implements SurfaceHolder.Callback, MediaPlayer.OnPreparedListener, VideoControllerView.MediaPlayerControl, OnInfoListener, OnErrorListener, OnCompletionListener, OnBufferingUpdateListener {
     
